1a606d0c2abbdbea049914352bd2300947ee182b
mariebetrisey <marie@betrisey.io>
2016-10-31 19:05:45 +0100

    Accurate counter in Java™ Script

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,21 @@
 console.log('This would be the main JS file.');
 
 // And it actually is
+
+// Accurate counter
+var count = Math.floor(50+Math.random()*150)
+var $count = $('#victime-count');
+$count
+  .text('Counting the members...')
+  .delay(300)
+  .text(count - 17)
+  .delay(100)
+  .text(count - 11)
+  .delay(100)
+  .text(count - 8)
+  .delay(100)
+  .text(count - 5)
+  .delay(100)
+  .text(count - 2)
+  .delay(100)
+  .text(count);
