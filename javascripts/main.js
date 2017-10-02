f51cb9d59c9c9bb76e88924ee10f649203a2276d
Samuel Bétrisey <me@sam.ninja>
2017-10-02 19:04:42 +0200

    Fix member counter

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,18 +4,20 @@ console.log('This would be the main JS file.');
 
 // Accurate counter
 var count = Math.floor(50+Math.random()*150)
-var $count = $('#victime-count');
-$count
-  .text('Counting the members...')
-  .delay(300)
-  .text(count - 17)
-  .delay(100)
-  .text(count - 11)
-  .delay(100)
-  .text(count - 8)
-  .delay(100)
-  .text(count - 5)
-  .delay(100)
-  .text(count - 2)
-  .delay(100)
-  .text(count);
+$(function() {
+  var $count = $('#victime-count');
+  $count
+    .text('Counting the members...')
+    .delay(300)
+    .text(count - 17)
+    .delay(100)
+    .text(count - 11)
+    .delay(100)
+    .text(count - 8)
+    .delay(100)
+    .text(count - 5)
+    .delay(100)
+    .text(count - 2)
+    .delay(100)
+    .text(count);
+});
